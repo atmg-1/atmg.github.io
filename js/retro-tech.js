4de01cd972c2c112d6e2933639d1419dba73467d
Your Name <you@example.com>
2026-01-13 14:38:15 +0800

    Site updated: 2026-01-13 14:38:14

--- js/retro-tech.js
+++ js/retro-tech.js
@@ -158,50 +158,30 @@ hackBlinkStyle.textContent = `
   
   @keyframes glitch-anim {
     0% { clip: rect(42px, 9999px, 44px, 0); }
-    5% { clip: rect(12px, 9999px, 59px, 0); }
-    10% { clip: rect(48px, 9999px, 29px, 0); }
-    15% { clip: rect(42px, 9999px, 73px, 0); }
-    20% { clip: rect(63px, 9999px, 27px, 0); }
-    25% { clip: rect(34px, 9999px, 55px, 0); }
-    30% { clip: rect(86px, 9999px, 73px, 0); }
-    35% { clip: rect(20px, 9999px, 20px, 0); }
-    40% { clip: rect(26px, 9999px, 60px, 0); }
-    45% { clip: rect(25px, 9999px, 66px, 0); }
-    50% { clip: rect(57px, 9999px, 98px, 0); }
-    55% { clip: rect(5px, 9999px, 46px, 0); }
-    60% { clip: rect(82px, 9999px, 31px, 0); }
-    65% { clip: rect(54px, 9999px, 27px, 0); }
-    70% { clip: rect(28px, 9999px, 99px, 0); }
-    75% { clip: rect(45px, 9999px, 69px, 0); }
-    80% { clip: rect(23px, 9999px, 85px, 0); }
-    85% { clip: rect(54px, 9999px, 84px, 0); }
-    90% { clip: rect(45px, 9999px, 47px, 0); }
-    95% { clip: rect(37px, 9999px, 20px, 0); }
-    100% { clip: rect(73px, 9999px, 99px, 0); }
+    10% { clip: rect(12px, 9999px, 59px, 0); }
+    20% { clip: rect(48px, 9999px, 29px, 0); }
+    30% { clip: rect(42px, 9999px, 73px, 0); }
+    40% { clip: rect(63px, 9999px, 27px, 0); }
+    50% { clip: rect(34px, 9999px, 55px, 0); }
+    60% { clip: rect(86px, 9999px, 73px, 0); }
+    70% { clip: rect(20px, 9999px, 20px, 0); }
+    80% { clip: rect(26px, 9999px, 60px, 0); }
+    90% { clip: rect(25px, 9999px, 66px, 0); }
+    100% { clip: rect(57px, 9999px, 98px, 0); }
   }
   
   @keyframes glitch-anim2 {
     0% { clip: rect(65px, 9999px, 100px, 0); }
-    5% { clip: rect(52px, 9999px, 74px, 0); }
-    10% { clip: rect(79px, 9999px, 85px, 0); }
-    15% { clip: rect(75px, 9999px, 5px, 0); }
-    20% { clip: rect(67px, 9999px, 61px, 0); }
-    25% { clip: rect(14px, 9999px, 79px, 0); }
-    30% { clip: rect(1px, 9999px, 66px, 0); }
-    35% { clip: rect(86px, 9999px, 30px, 0); }
-    40% { clip: rect(23px, 9999px, 98px, 0); }
-    45% { clip: rect(85px, 9999px, 72px, 0); }
-    50% { clip: rect(71px, 9999px, 75px, 0); }
-    55% { clip: rect(28px, 9999px, 84px, 0); }
-    60% { clip: rect(45px, 9999px, 54px, 0); }
-    65% { clip: rect(38px, 9999px, 19px, 0); }
-    70% { clip: rect(58px, 9999px, 87px, 0); }
-    75% { clip: rect(70px, 9999px, 60px, 0); }
-    80% { clip: rect(18px, 9999px, 53px, 0); }
-    85% { clip: rect(72px, 9999px, 100px, 0); }
-    90% { clip: rect(40px, 9999px, 85px, 0); }
-    95% { clip: rect(91px, 9999px, 74px, 0); }
-    100% { clip: rect(12px, 9999px, 80px, 0); }
+    10% { clip: rect(52px, 9999px, 74px, 0); }
+    20% { clip: rect(79px, 9999px, 85px, 0); }
+    30% { clip: rect(75px, 9999px, 5px, 0); }
+    40% { clip: rect(67px, 9999px, 61px, 0); }
+    50% { clip: rect(14px, 9999px, 79px, 0); }
+    60% { clip: rect(1px, 9999px, 66px, 0); }
+    70% { clip: rect(86px, 9999px, 30px, 0); }
+    80% { clip: rect(23px, 9999px, 98px, 0); }
+    90% { clip: rect(85px, 9999px, 72px, 0); }
+    100% { clip: rect(71px, 9999px, 75px, 0); }
   }
 `;
 document.head.appendChild(hackBlinkStyle);
